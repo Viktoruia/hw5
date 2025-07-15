082d4d71862ff99911ccaf7cd2a4b304218a3b9e
Виктория <victoriaklisova@gmail.com>
2025-07-15 23:41:49 +0300

    add task2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,4 +8,25 @@ enum Season {
     case spring
     case summer
     case autumn
+    case nonSeason
 }
+
+//2. Написать функцию, которая будет принимать номер месяца (Int) и возвращать enum
+//с временем года этого месяца.
+
+func season(numberMonth: Int) -> Season {
+    switch numberMonth {
+    case 1, 2, 12:
+        return .winter
+    case 3, 4, 5:
+        return .spring
+    case 6, 7, 8:
+        return .summer
+    case 9, 10, 11:
+        return .autumn
+    default:
+        return .nonSeason
+    }
+}
+
+print("Пора года \(season(numberMonth: 10))")
